200fef3daf03c56fd79bca32fede72f140f13e42
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2025-01-11 16:46:13 +0200

    Update visualization.py

--- DL_project/visualization.py
+++ DL_project/visualization.py
@@ -22,30 +22,6 @@ def plot_sentiment_distribution(data, class_names):
     plt.yticks(fontsize=12)
     plt.show()
 
-
-# Visualization: Text Length Distribution
-def visualize_text_length(data, title):
-    """
-    Visualize the distribution of text lengths in the dataset.
-
-    Args:
-        data (DataFrame): The preprocessed dataset.
-        title (str): Title for the plot.
-    """
-    data['text_length'] = data['Text'].apply(len)
-
-    plt.figure(figsize=(8, 4))
-    custom_font = FontProperties(family='serif', style='normal', size=14, weight='bold')
-
-    plt.hist(
-        data['text_length'], bins=40, color='lightcoral', edgecolor='black', alpha=0.7, label='Text Length'
-    )
-    plt.grid(linestyle='--', alpha=0.6)
-    plt.xlabel("Text Length", fontsize=10, fontproperties=custom_font, color='black')
-    plt.ylabel("Frequency", fontsize=10, fontproperties=custom_font, color='black')
-    plt.title(f'Text Length Distribution for {title}', fontsize=12, fontproperties=custom_font, color='black')
-    plt.show()
-
 # Visualization: Training and Validation Metrics
 def plot_training_metrics(history):
     """
